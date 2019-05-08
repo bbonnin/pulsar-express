a7cf6dbdbded3d608bf8a6b0b8bafc1ed9496976
Bruno Bonnin <bbonnin@gmail.com>
2019-05-08 16:24:22 +0200

    Initial commit

---
+++ pages/connections.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dataview">
+    <el-alert
+      title="Missing features :( Keep calm and drink tea !"
+      type="error"
+      show-icon>
+  </el-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'dataview',
+}
+</script>
+
+<style scoped>
+  
+</style>
