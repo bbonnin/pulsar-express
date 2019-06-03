b73fedba9076be3b2f112b0b423fc6ea8dc22e84
Bruno Bonnin <bbonnin@gmail.com>
2019-06-03 16:42:49 +0200

    Change breadcrumb and look

---
+++ layouts/simple-dataview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav-bar />
+    <div class="simple-dataview-container">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/navbar'
+
+export default {
+  components: { NavBar },
+}
+</script>
+
+<style>
+.simple-dataview-container {
+  padding: 20px;
+  width: 80%;
+  margin: 4rem auto;
+}
+</style>
--- pages/overview.vue
+++ pages/overview.vue
@@ -3,20 +3,20 @@
     <h2 class="title">Overview</h2>
     <el-row :gutter="12">
       <el-col :span="8">
-        <el-card shadow="always">
-          <h3 class="subtitle">Clusters</h3>
+        <el-card shadow="never">
+          <h3 class="subtitle"><img src="/clusters.png" width="32" />Clusters</h3>
           <el-button type="text" class="big-number" @click.native.prevent="$router.push({ path: '/clusters' })">{{ clusters.length }}</el-button>
         </el-card>
       </el-col>
       <el-col :span="8">
-        <el-card shadow="always">
-          <h3 class="subtitle">Brokers</h3>
+        <el-card shadow="never">
+          <h3 class="subtitle"><img src="/brokers.png" width="32" />Brokers</h3>
           <el-button type="text" class="big-number" @click.native.prevent="$router.push({ path: '/brokers' })">{{ brokers.length }}</el-button>
         </el-card>
       </el-col>
       <el-col :span="8">
-        <el-card shadow="always">
-          <h3 class="subtitle">Topics</h3>
+        <el-card shadow="never">
+          <h3 class="subtitle"><img src="/topics.png" width="32" />Topics</h3>
           <el-button type="text" class="big-number" @click.native.prevent="$router.push({ path: '/topics' })">{{ topics.length }}</el-button>
         </el-card>
       </el-col>
@@ -30,7 +30,7 @@ import { mapState } from 'vuex'
 export default {
   name: 'overview',
 
-  layout: 'dataview',
+  layout: 'simple-dataview',
 
   data() {
     return {
@@ -71,10 +71,15 @@ export default {
 .subtitle {
   font-weight: 300;
   color: #2c3e50;
+  line-height: 1.8;
 }
 .big-number {
   font-size: 80px;
   padding: 20px;
   width: 100%;
+}
+img {
+  float: left;
+  margin-right: 20px;
 }
 </style>
